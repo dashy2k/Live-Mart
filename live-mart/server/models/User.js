233beb7d2211f41f9e5e353e5b414b8794132a2d
Hardik Bhalla <hardikbhalla03@gmail.com>
2021-04-23 01:44:07 +0530

    Fixed the signup

--- live-mart/server/models/User.js
+++ live-mart/server/models/User.js
@@ -1,20 +1,35 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-    googleId : String,
-    name : String,
-    picture : String,
-    email : String,
-    cart:{
-        type: Array,
-        default:[]
 
-    },
-    history:{
+const userSchema = new Schema({
+    googleId: String,
+    name: String,
+    picture: String,
+    email: String,
+    cart: {
         type: Array,
         default: []
+
+    },
+    history: {
+        type: Array,
+        default: []
+    },
+    signUpDetails: {
+        firstName: String,
+        lastName: String,
+        email: String,
+        password: String,
+        re_password: String,
+        customer: Boolean,
+        retailer: Boolean,
+        wholesaler: Boolean,
+        address1: String,
+        address2: String,
+
+
     }
 })
 
-mongoose.model('users',userSchema);
+mongoose.model('users', userSchema);
--- live-mart/server/routes/signup.js
+++ live-mart/server/routes/signup.js
@@ -1,19 +1,14 @@
 
 const bodyParser = require('body-parser');
 
-
-
 module.exports = app => {
     app.use(bodyParser.urlencoded({ extended: true }));
-    app.get('/usersignup', (req, res) => {
-        res.send("<h1>On the signup page</h1>");
-        console.log("Hello there");
-    })
 
     app.post('/usersignup', (req, res) => {
-        res.send("Thanks for posting that");
+        //res.send("Thanks for posting that");
         console.log(req.body);
         var userDetails = JSON.stringify(req.body);
         console.log(userDetails);
+        res.redirect('http://localhost:3000');
     })
 }
